0ffb755e84720a3279efba62327cd2cb681a6054
ALU201719628 <ALU201719628@F7-LAB201-1381.uniritter.local>
2019-05-21 22:07:42 -0300

    opa

---
+++ receita-master/Receita/src/ReceitaMonstro.java
@@ -0,0 +1,10 @@
+
+public class ReceitaMonstro implements IReceita {
+	
+	@Override
+	public String receita() {
+		// TODO Auto-generated method stub
+		return "Nove da matina; Frango batata doce; Ficar monstrão";
+	}
+
+}
---
+++ receita-master/Receita/src/TesteReceita.java
@@ -0,0 +1,12 @@
+
+public class TesteReceita {
+
+	public static void main(String[] args) {
+		IReceita bolo = new ReceitaBolo();
+		System.out.println(bolo.receita());
+		ReceitaMonstro monstro = new ReceitaMonstro();
+		System.out.println(monstro.receita());
+		
+	}
+	
+}
